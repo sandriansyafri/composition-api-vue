ad154a94495039a477da7d8e576be546e55eee55
sandriannsyafri <sandriannsyafri@gmail.com>
2021-09-23 14:09:15 +0700

    composition API using Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home container">
+        <div class="row justify-content-center">
+            <div class="col-6 text-center">
+                <h1>Search</h1>
+                <input v-model="keywords" type="text" class="form-control" />
+            </div>
+        </div>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-6">
+                <h3 class="text-center">Lists Student</h3>
+                <ul class="list-group" v-if="filterStudent.length">
+                    <li v-for="student of filterStudent" :key="student" class="list-group-item">{{ student }}</li>
+                </ul>
+                <ul v-else class="text-center">
+                    <li class="list-group-item">data tidak ada</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { computed, ref } from "@vue/reactivity";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    setup() {
+        const keywords = ref("");
+        const students = ref(["Hafid", "Yogi", "Fikri"]);
+        const filterStudent = computed(() => {
+            return students.value.filter((student) => {
+                return student.toLowerCase().includes(keywords.value.toLowerCase());
+            });
+        });
+
+        return {
+            students,
+            keywords,
+            filterStudent,
+        };
+    },
+};
 </script>
